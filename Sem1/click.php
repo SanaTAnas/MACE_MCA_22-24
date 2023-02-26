3c0c465b85a24229d911232f579bd62bc5705406
Sana T Anas <121151456+SanaTAnas@users.noreply.github.com>
2023-02-26 17:49:43 +0700

    Delete click.php

--- Sem1/click.php
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-	<title>random</title>
-</head>
-<body>
-	<h3>RANDOM NUMBER GENERATOR</h3>
-	<a href="Random_Generator.php" target="i_frame1">click here to create random numbers</a>
-</body>
