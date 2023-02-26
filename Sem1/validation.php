7c23b7b3b9f295945f0bcff4a7967f810208742c
Sana T Anas <121151456+SanaTAnas@users.noreply.github.com>
2023-02-26 13:10:59 +0700

    Add files via upload

---
+++ Sem1/validation.php
@@ -0,0 +1,33 @@
+<?php
+
+$aa=new mysqli("localhost","root","");
+
+if($aa->connect_error)
+
+{
+
+    die("connection failed".$aa->connect_error);
+
+}
+
+$db="create database stud23";
+
+if($aa->query($db)===TRUE)
+
+{
+
+    echo "Database created";
+
+}
+
+ else
+
+{
+
+     echo "error found".$aa->error;
+
+}
+
+$aa->close();
+
+?>
