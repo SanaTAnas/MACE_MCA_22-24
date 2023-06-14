193dd334108f041769d683614f1138f8440c6829
Sana T Anas <121151456+SanaTAnas@users.noreply.github.com>
2023-06-14 15:34:18 +0530

    Update resultMain.java

--- sem2/java/14/resultMain.java
+++ sem2/java/14/resultMain.java
@@ -1,23 +1,29 @@
 import java.util.*;
+
 class student
+    
 {
     int sNo;
     String sName;
+    
     student(int no,String nm)
     {
         sName=nm;
         sNo=no;
     }
+    
     void display()
     {
         System.out.println("Student Number:"+sNo);
         System.out.println("Student Name:"+sName);
     }
 }
+
 interface sport
 {
     void displaysports();
 }
+
 class result extends student implements sport
 {
     String result;
@@ -30,11 +36,13 @@ class result extends student implements sport
         this.sId=sId;
         this.item=item;
     }
+    
     public void displaysports()
     {
         System.out.println("Sports ID:"+sId);
         System.out.println("Sports Item:"+item);
     }
+    
     void display()
     {
         super.display();
@@ -42,7 +50,9 @@ class result extends student implements sport
         System.out.println("Result:"+result);
     }
 }
+
 class resultMain{
+
    public static void main(String arg[]){
       Scanner sc=new Scanner(System.in);
       System.out.println("Enter student no:");
