8a31c981ef5b9e2a3a58af5e9120578d4fee1b9a
Sana T Anas <121151456+SanaTAnas@users.noreply.github.com>
2023-12-12 11:10:39 +0530

    Update MainActivity.java

--- Sem3/ANDROID/SharedPreferences/MainActivity.java
+++ Sem3/ANDROID/SharedPreferences/MainActivity.java
@@ -1,42 +1,51 @@
-package com.example.shared_preferences;
+package com.example.sharedepreferences;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText name,email;
-    String a,b;
+    EditText uname, email;
+    String a, b;
     SharedPreferences sp;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sp = getSharedPreferences("MyPref",MODE_PRIVATE);
-        name = (EditText) findViewById(R.id.uname);
-        email = (EditText) findViewById(R.id.email);
+        sp = getSharedPreferences("MyPref", MODE_PRIVATE);
+        uname = findViewById(R.id.uname);
+        email = findViewById(R.id.email);
         if (sp.contains("Name"))
-            name.setText(sp.getString("Name",""));
+            uname.setText(sp.getString("Name", ""));
         if (sp.contains("Email"))
-            email.setText(sp.getString("Email",""));
+            email.setText(sp.getString("Email", ""));
     }
-    public void nextActivity(View v){
-        a = name.getText().toString();
+
+    public void nextActivity(View v) {
+        a = uname.getText().toString();
         b = email.getText().toString();
-        SharedPreferences.Editor editor = sp.edit();
-        editor.putString("Name",a);
-        editor.putString("Email",b);
-        editor.apply();
-        System.out.println("Next");
-        Intent i = new Intent(this,SecondActivity.class);
-        startActivity(i);
 
+        if (isValidEmail(b)) {
+            SharedPreferences.Editor editor = sp.edit();
+            editor.putString("Name", a);
+            editor.putString("Email", b);
+            editor.apply();
+            Intent i = new Intent(this, SecondActivity.class);
+            startActivity(i);
+        } else {
+            Toast.makeText(this, "Invalid email address", Toast.LENGTH_SHORT).show();
+        }
+    }
 
+    private boolean isValidEmail(CharSequence target) {
+        return Patterns.EMAIL_ADDRESS.matcher(target).matches();
     }
 }
